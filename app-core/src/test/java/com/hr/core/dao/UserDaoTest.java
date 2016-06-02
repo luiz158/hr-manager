e00d6635bb0eb13def1b2360049989df0cd1de3d
chathuranga tennakoon <chathuranga.tennakoon@gtnexus.com>
2016-06-02 17:08:32 +0530

    commiting testcases

---
+++ app-core/src/test/java/com/hr/core/dao/UserDaoTest.java
@@ -0,0 +1,62 @@
+package com.hr.core.dao;
+
+import com.hr.core.AppCoreGenericTests;
+import com.hr.core.model.User;
+import com.hr.core.model.UserRole;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.testng.Assert.*;
+
+public class UserDaoTest extends AppCoreGenericTests{
+
+    @Autowired
+    private UserDao userDao;
+
+    @Autowired
+    private UserRoleDao userRoleDao;
+
+    @BeforeMethod
+    public void setUp() throws Exception {
+        System.out.println("executing setUp method");
+    }
+
+    @AfterMethod
+    public void tearDown() throws Exception {
+        System.out.println("executing tearDown method");
+    }
+
+    @Test
+    public void testSaveUser(){
+        //create user role
+        List<UserRole> userRoleList = new ArrayList<>();
+        UserRole userRole1 =  createUserRole("ROlE_USER");
+        UserRole userRole2 =  createUserRole("ROlE_ADMIN");
+
+        userRoleList.add(userRole1);
+        userRoleList.add(userRole2);
+
+        User user = new User();
+        user.setUsername("chathuranga");
+        user.setPassword("chat123");
+        user.setRolesList(userRoleList);
+
+        userDao.save(user);
+
+        Assert.assertNotNull(user.getId());
+    }
+
+
+    private UserRole createUserRole(String roleName){
+        UserRole userRole = new UserRole();
+        userRole.setName(roleName);
+        userRoleDao.save(userRole);
+        assertNotNull(userRole.getId());
+        return userRole;
+    }
+}
---
+++ app-core/src/test/java/com/hr/core/dao/UserRoleDaoTest.java
@@ -0,0 +1,13 @@
+package com.hr.core.dao;
+
+import com.hr.core.AppCoreGenericTests;
+import org.testng.annotations.Test;
+import static org.testng.Assert.*;
+
+public class UserRoleDaoTest extends AppCoreGenericTests{
+
+    @Test
+    public void testCreateUserRole(){
+        System.out.printf("create user role");
+    }
+}
